2e80e2c0326c378e02f0d22446b9924218dc55f3
GiftofHermes <aka_polarbear@hotmail.com>
2023-03-11 01:48:27 +0100

    forgot dash

--- rust_cli_tools.sh
+++ rust_cli_tools.sh
@@ -16,7 +16,7 @@ echo 'alias l="exa -l --git"' >> ~/.bashrc
 echo 'alias la="exa -l -a --git"' >> ~/.bashrc
 echo 'alias ll="exa -l -g -a --git"' >> ~/.bashrc
 echo 'alias llt="exa -1 --tree --git-ignore"' >> ~/.bashrc
-echo 'alias llr="exa -l -recurse --level=2"' >> ~/.bashrc
+echo 'alias llr="exa -l --recurse --level=2"' >> ~/.bashrc
 
 cargo install ripgrep
 cargo install --locked bat
